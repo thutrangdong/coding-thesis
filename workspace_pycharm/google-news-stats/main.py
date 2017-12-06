7c1867f3fbf29ac012e9ad1a93181b0de085387a
thutrangdong <thutrang.dong@student.HTW-Berlin.de>
2017-12-06 11:03:51 +0100

    no message

--- workspace_pycharm/google-news-stats/main.py
+++ workspace_pycharm/google-news-stats/main.py
@@ -8,19 +8,31 @@ ending_date = datetime.date(2011, 5, 6)
 delta = ending_date - starting_date
 browser = webdriver.Firefox()
 
-for current_date in (starting_date + datetime.timedelta(n) for n in range(-1,delta.days)):
+starting_date = datetime.date(2011, 5, 1) #year, month, day
 
-    current_date_day = current_date.strftime('%-d')
-    current_date_month = current_date.strftime('%-m')
+ending_date = datetime.date(2017, 11, 18)
+delta = ending_date - starting_date
+browser = webdriver.Firefox()
+
+amount_publications = []
+
+for current_date in (starting_date + datetime.timedelta(n) for n in range(0,delta.days+1)):
+
+    current_date_day = current_date.strftime('%d')
+    current_date_month = current_date.strftime('%m')
     current_date_year = current_date.strftime('%Y')
-    base_url = "https://www.google.de/search?q=bitcoin&biw=1680&bih=926&source=lnt&tbs=cdr:1,cd_min:"+current_date_month+"/"+current_date_day+"/"+current_date_year+",cd_max:"+current_date_month+"/"+current_date_day+"/"+current_date_year+"&tbm=nws&gws_rd=cr&dcr=0&ei=ZvEmWofaEYTFwALMh7PICQ"
+    base_url = "https://www.genios.de/dosearch?explicitSearch=true&q=bitcoin&searchRestriction=&dbShortcut=%3A2%3AALLEQUELLENNEU-1_%3A2%3AALLEQUELLENNEU&searchMask=5461&TI%2CUT%2CDZ%2CBT%2COT%2CSE%2CSL=&NN%2CAU%2CMM%2CZ2=&CO%2CC2%2CTA%2CKA%2CVA%2CZ1=&CT%2CZ4%2CKW=&BR%2CGW%2CN1%2CN2%2CNC%2CND%2CSC%2CWZ%2CZ5%2CAI%2CBC%2CKN%2CTN%2CVN%2CK0%2CB4=&Z3%2CCN%2CCE%2CKC%2CTC%2CVC=&timeFilterType=on&DT_from="+current_date_day+"."+current_date_month+"."+current_date_year+"&DT_to="+current_date_day+"."+current_date_month+"."+current_date_year+"&x=70&y=13"
     browser.get(base_url)
     html = browser.page_source
     soup = BeautifulSoup(html, "html.parser")
-    items_current = soup.find("div", {"id": "resultStats"}).getText()
-    value = re.search('(?<=Ungefähr )(.*)(?= Ergebnisse)', items_current).group(0)
-    value = value.replace('.', '')
-    value = int(value)
-    print(value)
+    items_current = soup.find("div", {"class": "moduleResultTextHeader"}).getText()
+    #print(items_current)
+    if items_current == "Ihre Suchanfrage ergab 0 Treffer":
+        value = 0
+    else:
+        value = re.search('\(([^\)]+)\)', items_current).group(1)
+        value = int(value)
+    print(current_date.strftime('%d/%m/%Y') + "\t" + str(value))
+    amount_publications.append(value)
 
     #^.*[ ,\t]+
